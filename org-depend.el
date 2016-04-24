6c1d790a2dd659f591a17ee094c8f5747f5d8080
Alexander Baier <alexander.baier@mailbox.org>
2016-04-24 20:37:16 +0200

    Add dependency graph and dot creation.

--- org-depend.el
+++ org-depend.el
@@ -34,3 +34,43 @@
      ((eq h1<h2 h1>h2) 'equal)
      (h1>h2 'smaller)
      (h1<h2 'greater))))
+
+(defun org-depend--merge-graphs (graphs)
+  (message "Merging:\n  %S" graphs)
+  (-reduce-from
+   (-lambda ((vs1 . es1) (vs2 . es2))
+             (cons (-union vs1 vs2) (-union es1 es2)))
+   '(() . ())
+   graphs))
+
+(defun org-depend-graph (id db)
+  (-if-let (deps (cdr (org-depend-get id db)))
+      (progn
+        (message "Looking at %S with deps: %S" id deps)
+        (let ((new-edges (-map (-partial #'cons id) deps))
+              (results (org-depend--merge-graphs
+                        (-map (-rpartial #'org-depend-graph db) deps))))
+          (cons (cons id (car results)) (append new-edges))))
+    (message "Looking at %S with no deps." id)
+    (cons (list id) '())))
+
+(defun org-depend-graphs (ids db)
+  (org-depend--merge-graphs
+   (-map (-rpartial #'org-depend-graph db) ids)))
+
+(defun org-depend-get-headline-from-id (id)
+  (with-current-buffer (find-file-noselect (car (org-id-find id)))
+    (goto-char (cdr (org-id-find id)))
+    (plist-get (cadr (org-element-at-point)) :title)))
+
+(defun org-depend-graph-to-dot (graph)
+  (let* ((edges (-map (-lambda ((from . to))
+                        (cons (org-depend-get-headline-from-id from)
+                              (org-depend-get-headline-from-id to))) (cdr graph)))
+         (edges-dot (-map (-lambda ((from . to))
+                            (concat from " -> " to ";"))
+                          edges)))
+    (s-join "\n"
+            (append '("digraph org_dependencies {")
+                    (-map (-partial #'s-prepend "  ") edges-dot)
+                    '("}")))))
