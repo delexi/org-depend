e0b518074889ed2a1807e4d2c6af6fead0a29068
Alexander Baier <alexander.baier@mailbox.org>
2016-05-01 18:02:31 +0200

    Build before package.

--- test-fresh-install.sh
+++ test-fresh-install.sh
@@ -7,6 +7,7 @@ ROOT=.
 DIST=dist
 TMP_HOME=/tmp/home-org-depend
 
+cask build
 cask package ${ROOT}/${DIST}
 HOME=$TMP_HOME cask emacs --eval \
 "(package-install-file \"${ROOT}/${DIST}/org-depend-${ORG_DEPEND_VERSION}.tar\")"
