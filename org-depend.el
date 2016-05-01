24eac2f13281bd9a7ecddaf07affe8435532120e
Alexander Baier <alexander.baier@mailbox.org>
2016-05-01 14:36:04 +0200

    Big commit.
    Existing commands:
    - Add dependency between two headlines.
    - Add dependency between headline and a newly captured one.
    - Show agenda with all dependencies of one headline.
    - Show a graph (graphviz) of the dependencies of one headline.

--- org-depend.el
+++ org-depend.el
@@ -1,38 +1,101 @@
-(require 'org)
-(require 'org-id)
+;; -*- lexical-binding: t -*-
+
+(require 'cl-lib)
 (require 'dash)
 (require 'dash-functional)
+(require 'org)
+(require 'org-id)
 
+(defconst org-depend-property "DEPEND")
+(defconst org-depend-symbol (intern (concat ":" org-depend-property)))
 
-(defvar org-depend-db (make-hash-table :test 'equal))
+(defun org-depend--db-init ()
+  (cons (make-hash-table :test 'equal) (make-hash-table :test 'equal)))
 
-(defun org-depend-db-init () (setq org-depend-db (make-hash-table :test 'equal)))
+(defvar org-depend-db (org-depend-db-init))
 
-(defun org-depend-get (id db) (cons id (gethash id db)))
-(defun org-depend-put (h db) (puthash (car h) (cdr h) db))
+(defvar org-depend-files org-refile-targets
+  "Files scanned for dependant headlines in the format of `org-refile-targets'.")
 
-(defun org-depend-plist-get-all (key plist)
-  (funcall (-compose
-            (-partial #'-keep (-lambda ((k v)) (when (eq k key) v)))
-            (-partial #'-partition 2))
-           plist))
+;;; inner helper functions
+(defun org-depend--db-put (id deps db) (puthash id deps (car db)))
+(defun org-depend--get-update-time (file db) (gethash file (cdr db)))
+(defun org-depend--put-update-time (time file db) (puthash file time (cdr db)))
 
-(defun org-depend-update-from-file (buffer db)
-  (with-current-buffer buffer
-    (org-element-map (org-element-parse-buffer 'headline) 'headline
-      (lambda (h)
-        (let* ((props (cadr h))
-               (id (org-id-get (plist-get props :begin) 'create))
-               (depends (org-depend-plist-get-all :DEPEND props)))
-          (org-depend-put (cons id depends) db)))))
-  db)
+(defun org-depend--get-depend-files ()
+  (-flatten
+   (-map
+    (-lambda ((fn-or-list . _))
+      (cond
+       ((listp fn-or-list) fn-or-list)
+       ((functionp fn-or-list) (let ((file (funcall fn-or-list)))
+                                 (if (listp file) file (list file))))
+       (t (error "Car of each cons in `org-depend-files' must be a function or a list"))))
+    org-depend-files)))
 
+(defun org-depend--get-dependencies (pom &optional buffer)
+  (assert (integer-or-marker-p pom))
+  (assert (bufferp buffer))
+  (let* ((pos (if (markerp pom) (marker-position pom) pom))
+         (buf (if (markerp pom) (marker-buffer pom) buffer)))
+    (with-current-buffer buf
+      (org-element-at-point)
+      (org-entry-get-multivalued-property pos org-depend-property))))
+
+(defun org-depend--add-dependency (dep-id pom &optional buffer)
+  (assert (stringp dep-id))
+  (assert (integer-or-marker-p pom))
+  (unless (markerp pom) (assert (bufferp buffer)))
+  (let* ((pos (if (markerp pom) (marker-position pom) pom))
+         (buf (if (markerp pom) (marker-buffer pom) buffer)))
+    (with-current-buffer buf
+      (org-element-at-point)
+      (org-entry-add-to-multivalued-property pos org-depend-property dep-id))))
+
+(defun org-depend--remove-dependency (dep-id pom &optional buffer)
+  (assert (stringp dep-id))
+  (assert (integer-or-marker-p dep-id))
+  (assert (bufferp buffer))
+  (let* ((pos (if (markerp pom) (marker-position pom)) pom)
+         (buf (if (markerp pom) (marker-buffer pom) buffer)))
+    (with-current-buffer buf
+      (org-element-at-point)
+      (org-entry-remove-from-multivalued-property pos org-depend-property dep-id))))
+
+(defun org-depend--pomoi-to-id (pomoi &optional create)
+  (pcase pomoi
+    ((pred integer-or-marker-p) (org-id-get pomoi create))
+    ((pred stringp)             pomoi)
+    (`nil                       (org-id-get (point) create))
+    (_                          nil)))
+
+(defun org-depend--pomoi-to-id-check (param pomoi &optional create)
+  (-if-let (id (org-depend--pomoi-to-id pomoi create))
+      id
+    (error (concat param " must be an integer, marker, string or nil"))))
+
+(defun org-depend--get-id-with-outline-path-completion (&optional targets)
+  "Use `outline-path-completion' to retrieve the ID of an entry.
+TARGETS may be a setting for `org-refile-targets' to define
+eligible headlines.  When omitted, all headlines in the current
+file are eligible.  This function returns the ID of the entry.
+If necessary, the ID is created."
+  (let* ((org-refile-targets (or targets '((nil . (:maxlevel . 10)))))
+         (org-refile-use-outline-path nil)
+         (org-refile-target-verify-function nil)
+         (spos (org-refile-get-location "Entry" nil nil 'no-excludes))
+         (pom (and spos (move-marker (make-marker) (nth 3 spos)
+                                     (get-file-buffer (nth 1 spos))))))
+    (prog1 (org-id-get pom 'create)
+      (move-marker pom nil))))
+
+;;; Dependency API
 (defun org-depend-find (f h db)
   (let ((res (funcall f h)))
     (if res
         res
-      (-when-let (depends (cdr (org-depend-get h db)))
-        (-some (lambda (h) (org-depend-find f h db)) depends)))))
+      (-when-let (dependencies (org-depend-db-get h db))
+        (-some (lambda (h) (org-depend-find f h db)) dependencies)))))
 
 (defun org-depend-compare (id1 id2 db)
   (let ((h1<h2 (org-depend-find (-partial #'equal id1) id2 db))
@@ -42,6 +105,82 @@
      (h1>h2 'smaller)
      (h1<h2 'greater))))
 
+(defun org-depend-db-get (id db)
+  (gethash id (car db)))
+
+(defun org-depend-db-get-transitive (id db)
+  (-when-let (dependencies (org-depend-db-get id db))
+    (append dependencies
+            (-flatten
+             (-map (-rpartial #'org-depend-db-get-transitive db)
+                   dependencies)))))
+
+(defun org-depend-sorting-strategy (a b)
+  (let* ((ma (or (get-text-property 0 'org-marker a)
+                 (get-text-property 0 'org-hd-marker a)))
+         (mb (or (get-text-property 0 'org-marker b)
+                 (get-text-property 0 'org-hd-marker b)))
+         (id-a (org-id-get ma 'create))
+         (id-b (org-id-get mb 'create)))
+    (cl-case (org-depend-compare id-a id-b org-depend-db)
+      ('equal nil)
+      ('greater 1)
+      ('smaller -1))))
+
+;;; DB update functionality
+(defun org-depend-update-db (file db)
+  (with-current-buffer (find-file-noselect file)
+    (org-map-entries
+     (lambda ()
+       (let* ((id (org-id-get (point)))
+              (dependencies
+               (and id
+                    (org-depend--get-dependencies (point) (current-buffer)))))
+         (when dependencies
+           (org-depend-db-put id dependencies db)
+           (org-depend-put-update-time (current-time) file db))))
+     t 'file))
+  db)
+
+(defun org-depend-update-db-from-files (files db &optional force)
+  (-map (lambda (file)
+          (let ((last-update-time (or (org-depend-get-update-time file db)
+                                      0))
+                (mod-time (nth 5 (file-attributes file))))
+            (when (or force
+                      (time-less-p last-update-time mod-time))
+              (org-depend-update-db file db))))
+        files)
+  db)
+
+(defun org-depend-update-all-files (&optional force)
+  (interactive "P")
+  (let ((all-files (org-depend--get-depend-files)))
+    (when (member (buffer-file-name) all-files)
+      (org-depend-update-db-from-files all-files org-depend-db force))))
+
+(defun org-depend-setup ()
+  (add-hook 'after-save-hook #'org-depend-update-all-files))
+
+
+;;; Capture onshot hook implementation
+(defvar org-depend--capture-oneshot-functions nil
+  "List of functions run by `org-depend--capture-hook-function'.")
+
+(defconst org-depend--oneshot-hook-variable 'org-capture-prepare-finalize-hook)
+
+(defun org-depend--capture-hook-function ()
+  "Runs all functions in `org-depend--capture-oneshot-functions' and
+then removes itself from `org-depend--oneshot-hook-variable'"
+  (-map #'funcall org-depend--capture-oneshot-functions)
+  (setq org-depend--capture-oneshot-hooks nil)
+  (remove-hook org-depend--oneshot-hook-variable #'org-depend--capture-hook-function))
+
+(defun org-depend--capture-add-oneshot-hook (&rest fns)
+  (setq org-depend--capture-oneshot-functions fns)
+  (add-hook org-depend--oneshot-hook-variable #'org-depend--capture-hook-function))
+
+;;; Graph functions
 (defun org-depend--merge-graphs (graphs)
   (-reduce-from
    (-lambda ((vs1 . es1) (vs2 . es2))
@@ -50,7 +189,7 @@
    graphs))
 
 (defun org-depend-graph (id db)
-  (-if-let (deps (cdr (org-depend-get id db)))
+  (-if-let (deps (org-depend-db-get id db))
       (let ((edges (-map (-partial #'cons id) deps))
             (results (org-depend--merge-graphs
                       (-map (-rpartial #'org-depend-graph db) deps))))
@@ -79,8 +218,59 @@
                     (-map (-partial #'s-prepend "  ") edges-dot)
                     '("}")))))
 
-(defun org-depend-show-dependency-graph (&optional buffer)
+;;; Commands
+(defun org-depend-agenda-dependencies (&optional transitive id-or-pom)
+  (interactive "P")
+  (setq id-or-pom (org-depend--pomoi-to-id id-or-pom 'create))
+  (unless id-or-pom
+    (error "id-or-pom must be an integer, marker, string or nil"))
+  (org-depend-update-db-from-files (org-depend--get-depend-files) org-depend-db)
+  (with-current-buffer (find-file-noselect (car (org-id-find id-or-pom)))
+    (goto-char (cdr (org-id-find id-or-pom)))
+    (let* ((dependencies
+            (if transitive
+                (org-depend-db-get-transitive id-or-pom org-depend-db)
+              (org-depend-db-get id-or-pom org-depend-db)))
+           (org-agenda-overriding-header
+            (format "Dependencies of: `%s'"
+                    (plist-get (cadr (org-element-at-point)) :title)))
+           (org-agenda-cmp-user-defined #'org-depend-sorting-strategy)
+           (org-agenda-sorting-strategy '(user-defined-down))
+           (org-agenda-files (list (buffer-file-name)))
+           (org-agenda-skip-function
+            (lambda () (if (member (org-id-get (point)) dependencies)
+                           nil
+                         (outline-next-heading)))))
+      (org-tags-view nil "{.*}"))))
+
+(defun org-depend-capture-dependency (&optional pomoi capture-keys)
   (interactive)
+  (let* ((from (org-depend--pomoi-to-id-check "pomoi" pomoi 'create))
+         (add-dependency-at-point
+          (lambda ()
+            (message "in-hook:\n  buffer: %S\n  point: %S" (current-buffer) (point))
+            (unless (eq (car (org-element-at-point)) 'headline)
+              (outline-back-to-heading 'invisible-ok))
+            (org-depend-add-dependency from (point)))))
+    (org-depend-update-db-from-files (org-depend--get-depend-files) org-depend-db)
+    (org-depend--capture-add-oneshot-hook add-dependency-at-point)
+    (org-capture nil capture-keys)))
+
+(defun org-depend-add-dependency (&optional from to)
+  (interactive "P")
+  (setq to (if (interactive-p)
+               (org-depend-get-id-with-outline-path-completion nil)
+             (org-depend--pomoi-to-id-check "to" to 'create)))
+  (setq from (if (eq from '(4))
+                 (org-depend-get-id-with-outline-path-completion nil)
+               (org-depend--pomoi-to-id-check "from" from 'create)))
+  (org-depend-update-db-from-files
+   (org-depend--get-depend-files) org-depend-db)
+  (org-depend--add-dependency to (org-id-find from 'as-marker)))
+
+(defun org-depend-show-dependency-graph (&optional buffer)
+  (interactive "bBuffer: ")
+  (org-depend-update-db-from-files (org-depend--get-depend-files) org-depend-db)
   (unless buffer (setq buffer (current-buffer)))
   (let* ((ids (with-current-buffer buffer
                 (org-map-entries (lambda () (org-entry-get (point) "ID")))))
@@ -100,8 +290,12 @@
     (image-transform-fit-to-height)
     (image-transform-fit-to-width)))
 
-(defun org-depend-add-dependency ()
-  (interactive)
-  (let ((from (point))
-        (to-id (org-id-get-with-outline-path-completion)))
-    (org-entry-put from "DEPEND" to-id)))
+;;; test stuff
+(defmacro org-depend-ignore-body (&rest body)
+  nil)
+
+(org-depend-ignore-body
+ (setq org-depend-files
+       (cons `(,(concat (file-name-directory (buffer-file-name)) "test.org") :maxlevel . 4)
+             org-depend-files))
+ )
